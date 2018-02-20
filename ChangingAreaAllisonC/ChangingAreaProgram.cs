63cd92986517beee10246819412b44045ee48585
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-02-20 14:16:51 -0500

    Initial commit

---
+++ ChangingAreaAllisonC/ChangingAreaProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangingAreaAllisonC
+{
+    static class ChangingAreaProgram
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ChangingAreaForm());
+        }
+    }
+}
